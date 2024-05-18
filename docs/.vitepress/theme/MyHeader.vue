56fd434b301036fc2d4dfa52f77b507975645141
Laix <894169287@qq.com>
2024-05-18 16:46:35 +0800

    article header update

--- docs/.vitepress/theme/MyHeader.vue
+++ docs/.vitepress/theme/MyHeader.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { useData } from 'vitepress';
-const { title } = useData()
+const { title, frontmatter } = useData()
 </script>
 
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <div :class="$style.info" v-if="frontmatter.tags">
+            <label :class="$style.label" for="time">标签:</label>
+            <span name="time">
+                <span class="tag" v-for="tag in frontmatter.tags">{{ tag }}</span>
+            </span>
+        </div>
+        <div :class="$style.info" v-if="frontmatter.date">
+            <label :class="$style.label" for="time">时间:</label>
+            <span name="time">{{ frontmatter.date }}</span>
+        </div>
     </header>
 </template>
 
@@ -17,5 +27,14 @@ h1 {
     position: relative;
     font-weight: 600;
     outline: none;
+}
+
+.info {
+    padding: 0.5em 0;
+}
+
+.label {
+    width: fit-content;
+    margin-right: 1em;
 }
 </style>
